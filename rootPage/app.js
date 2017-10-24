0448cf8052373db31a04a52ed4442544467e473a
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2017-10-24 15:38:02 -0200

    aplicativo melhorado

--- rootPage/app.js
+++ rootPage/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const pg = require('pg');
+const mustache = require('mustache');
 
 const app = express();
 
@@ -12,7 +13,23 @@ const connection = new pg.Client('postgresql://' + dbLocation + ':' + dbPort + '
 app.use(express.static(__dirname + '/static'));
 
 
+var paginasReais = (req,res) => {
+	res.send('qualquer merda aleatoria');
+	res.end();
+}
+
+var discriminacaoAcesso = function(req,res,next){
+	console.log(req.url);
+	next();
+	if(/^dacc/.test(req.url)){
+
+	}
+}
+
+app.use(discriminacaoAcesso);
+app.get('*', paginasReais);
 
 
 
 app.on('end', function() { connection.end();});
+app.listen(8080);
