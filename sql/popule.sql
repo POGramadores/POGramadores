770280ba3aff461ecf43dca0b63ceb5f8934973a
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2017-10-24 12:24:43 -0200

    criando banco

--- sql/popule.sql
+++ sql/popule.sql
@@ -1,6 +1,5 @@
-CREATE TYPE tipo AS ENUM ('ELOGIO','CRITICA','SUGESTAO','DUVIDAS');
-CREATE TYPE asssunto AS ENUM ('PROFESSOR', 'INSTITUTO', 'DISCIPLINA');
-
+CREATE TYPE tipo_mensagem AS ENUM ('ELOGIO','CRITICA','SUGESTAO','DUVIDAS');
+CREATE TYPE assunto AS ENUM ('PROFESSOR', 'INSTITUTO', 'DISCIPLINA');
 
 CREATE TABLE aluno(
 	email VARCHAR(45) PRIMARY KEY,
@@ -16,23 +15,24 @@ CREATE TABLE professor(
 	hash_senha CHAR(23) NOT NULL
 );
 
+CREATE TABLE professores(
+	id BIGSERIAL PRIMARY KEY,
+	nome VARCHAR(60) NOT NULL
+);
+
 CREATE TABLE mensagem(
 	id BIGSERIAL PRIMARY KEY,
-	tipo TIPO NOT NULL,
+	tipo TIPO_MENSAGEM NOT NULL,
 	assunto ASSUNTO NOT NULL,
-	professor VARCHAR(45) REFERENCES professores(id),
+	professor BIGSERIAL REFERENCES professores(id),
 	coordenacao_acesso BOOLEAN NOT NULL,
 	diretorio_academico_acesso BOOLEAN NOT NULL,
 	corpo VARCHAR(5000) NOT NULL,
 	titulo VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE professores(
-	id BIGSERIAL PRIMARY KEY,
-	nome VARCHAR(60) NOT NULL,
-);
 
-CREATE TABLE interage_com(
+CREATE TABLE envia(
 	aluno VARCHAR(45) REFERENCES aluno(email),
 	mensagem BIGSERIAL REFERENCES mensagem(id),
 	PRIMARY KEY (aluno,mensagem)
