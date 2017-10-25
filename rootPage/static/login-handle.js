6dfbb566a5ba4e34bb666f3606558295cbfb941e
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2017-10-25 02:57:34 -0200

    talvez tenha consertado essa defecação

--- rootPage/static/login-handle.js
+++ rootPage/static/login-handle.js
@@ -8,7 +8,7 @@ function tentarLogin(){
     var URL = "/login";
     var dados = "usuario=" + login + "&senha=" + senha;
     var async = false;
-    var request = new XMLHttpRequest;
+    var request = new XMLHttpRequest();
     request.onload = function () {
         var status = request.status;
         var resultado = request.responseText;
