de8fc3816ba22a49a87804b6f5514b81ffb503b6
BuildTools <victorcarneiro2009@gmail.com>
2017-10-25 09:04:20 -0200

    Alterações no Login, aprendi JQuery e refiz o iscripiti, mas tá com erro do PG

--- rootPage/static/login-handle.js
+++ rootPage/static/login-handle.js
@@ -1,35 +1,49 @@
 
 function tentarLogin(){
-    alert("ESSA MERDA NUM FUNFA!!!! CONSERTA AI");
-    var form = document.forms["login"];
-    var login = form["usuario"];
-    var senha = form["senha"];
-    var metodo = "POST";
-    var URL = "/login";
-    var dados = "usuario=" + login + "&senha=" + senha;
-    var async = false;
-    var request = new XMLHttpRequest();
-    request.onload = function () {
-        var status = request.status;
-        var resultado = request.responseText;
-    }
-    
-    request.open(method, URL, async);
-    request.send(dados);
-    alert(status);
-    if(status == 200){
-        var resultadoObj = JSON.parse(resultado);
-        request = new XMLHttpRequest;
-        metodo = "GET";
-        request.setRequestHeader("Authorization", resultadoObj.auth);
-        if(resultadoObj.tabela == "aluno"){
-            URL = "aluno-principal.html";
-        }else if(resultadoObj.tabela == "professor"){
-            URL = "professor-principal.html";
-        }else if(resultadoObj.tabela == "coordenador"){
-            URL = "coordenador-principal.html";
-        }
-        request.open(method, URL, async);
-        alert("Resultado: " + status + "\nDados:" + resultado);
+    try{
+        alert("TÁ BUGADO O SCRIPT, OBRIGADU");
+        $.post("/login",
+               "usuario=" + $("#usuario").value + "&senha=" + $("#senha").value,
+               function(data, status){
+                    if(status == 200){
+                        var resultadoObj = JSON.parse(data);
+                        if(resultadoObj.tabela == "aluno"){
+                            URL = "aluno-principal.html";
+                        }else if(resultadoObj.tabela == "professor"){
+                            URL = "professor-principal.html";
+                        }else if(resultadoObj.tabela == "coordenador"){
+                            URL = "coordenador-principal.html";
+                        }
+                        window.location.href = URL;
+                    }else if(status == 400){
+                        $("#erro-senha").slideDown(250);
+                        setTimeout(function () {
+                            $("#erro-senha").slideUp(500);
+                        }, 5000);
+                    }
+               }
+               );
+        return false;
+        //alert("ESSA MERDA NUM FUNFA!!!! CONSERTA AI");
+        /*var form = document.forms["login"];
+        var login = form["usuario"];
+        var senha = form["senha"];
+        var metodo = "POST";
+        var URL = "/login";
+        var dados = "usuario=" + login + "&senha=" + senha;
+        var request = new XMLHttpRequest();
+        request.onload = function () {
+            var status = request.status;
+            var resultado = request.responseText;
+            alert(status);
+            
+        };*/
+        /*
+        request.open(method, URL);
+        request.send(dados);
+        */
+    }catch(err){
+        console.log(err);
+        return false;
     }
 }
