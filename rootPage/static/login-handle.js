7a25715f792305872f849171bc3afa93c860098f
unknown <beatriz.tantow@gmail.com>
2017-10-25 13:29:23 -0200

    Desambiguação

--- rootPage/static/login-handle.js
+++ rootPage/static/login-handle.js
@@ -3,13 +3,21 @@ function tentarLogin(){
     try{
         $.post("/login", $("#login-form").serialize(),
                function(data, status){
-                    var resultadoObj = data;
-                    if(resultadoObj.tabela == "aluno"){
-                        URL = "aluno-principal.html";
-                    }else if(resultadoObj.tabela == "professor"){
-                        URL = "professor-principal.html";
-                    }else if(resultadoObj.tabela == "coordenador"){
-                        URL = "coordenador-principal.html";
+                    var auths = Object.keys(data)
+                    if(auths.length == 1){
+                        var resultadoObj = data;
+                        if(resultadoObj.tabela == "aluno"){
+                            URL = "aluno-principal.html";
+                        }else if(resultadoObj.tabela == "professor"){
+                            URL = "professor-principal.html";
+                        }else if(resultadoObj.tabela == "coordenador"){
+                            URL = "coordenador-principal.html";
+                        }else if(resultadoObj.tabela == "membro-dacc"){
+                            URL = "membro-dacc-principal.html";
+                        }
+                    }else{
+                        global.auths = auths;
+                        URL = "desambiguacao.html";
                     }
                     window.location.href = URL;
                }).fail(function(jqobj, status, error){
@@ -41,4 +49,19 @@ function enviarCadastro(){
     }
 }
 
-$('#entrar').click(tentarLogin);
+function desambigua(tipo){
+    var auths = global.auths;
+    var auth_selecionado = auths[tipo];
+    var URL;
+    document.cookie = "auth=" + auth_selecionado;
+    if(auth_selecionado == "aluno"){
+        URL = "aluno-principal.html";
+    }else if(auth_selecionado == "professor"){
+        URL = "professor-principal.html";
+    }else if(auth_selecionado == "coordenador"){
+        URL = "coordenador-principal.html";
+    }else if(auth_selecionado == "membro-dacc"){
+        URL = "membro-dacc-principal.html";
+    }
+    window.location.href = URL;
+}
